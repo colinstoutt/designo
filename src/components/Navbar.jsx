fee48c9517e31eeff52ed5d40330c451858eee99
cps97 <colinstoutt@gmail.com>
2023-04-05 18:10:35 -0700

    create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav className="container">Navbar</nav>;
+  return (
+    <nav className="container display-f jc-sb">
+      <div className="display-f ai-c">
+        <span className="oval"></span>
+        <h1 className="logo-text">Designo</h1>
+      </div>
+      <div>
+        <a className="nav-link font-h3 text-black" href="#">
+          Our Company
+        </a>
+        <a className="nav-link font-h3 text-black" href="#">
+          Locations
+        </a>
+        <a className="nav-link font-h3 text-black" href="#">
+          Contact
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
